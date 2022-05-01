2a253ccd2aedcfe9391a142b773f142d72e1a1a6
Virendra kumar dwivedi <101566111+Virendrakumardwivedi@users.noreply.github.com>
2022-05-01 08:46:22 +0530

    react

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import  {navbar} from '../components/navbar'
+//console.log("afsgdf:",afsgdf)
+//console.log('navbar:',navbar)
+
+import '../styles/navbar.css';
+
+
+let navbar_div = document.getElementById('navbar')
+navbar_div.innerHTML= navbar()
+
